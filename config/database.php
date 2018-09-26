1e0ef646129721ace8ad62d1c0690cce6825db18
Robison Machado <robisonpmachado@gmail..com>
2018-09-26 19:25:08 -0400

    adicionado configuração do banco online

--- config/database.php
+++ config/database.php
@@ -43,9 +43,17 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
+
             'database' => env('DB_DATABASE', 'cemiterio'),
             'username' => env('DB_USERNAME', 'cemiterio'),
             'password' => env('DB_PASSWORD', 'cemiterio@marataizes'),
+
+           /*  'database' => env('DB_DATABASE', 'cemiterio_marataizes'),
+            'username' => env('DB_USERNAME', 'cemiterio'),
+            'password' => env('DB_PASSWORD', 'cemiterio@marataizes'), 
+            */
+
+
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
